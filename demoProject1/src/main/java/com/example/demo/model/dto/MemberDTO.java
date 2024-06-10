2b923135b1d7f1069c5429f4bc089a60a35b3762
오재진 <130115836+renewjin@users.noreply.github.com>
2024-06-10 19:18:33 +0900

    Spring Boot 시작

---
+++ demoProject1/src/main/java/com/example/demo/model/dto/MemberDTO.java
@@ -0,0 +1,35 @@
+package com.example.demo.model.dto;
+
+/*
+ * Getter와 Setter 는 Lombok 라이브러리 활용해서
+ * 길게 작성하지 않음
+ * 기본생성자 
+ * 
+ * */
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@ToString
+public class MemberDTO {
+	// 나중에 db랑 연결할 모델들 작성
+//필드
+	private String memberId;
+	private String memberPw;
+	private String memerName;
+	private int memberAge;
+//메서드
+	//Getter   -> lombok @Getter 자동완성 사용하기
+	//Setter   -> lombok @Setter 자동완성 사용하기
+	//기본생성자  -> lombok @NoArgsConstructor 자동완성 사용하기
+	//필수생성자  -> lombok @AllArgsConstructor 자동완성 사용하기
+	//toString  -> lombok @ToString           자동완성 사용하기
+}
+
+
+
+
+
+
+
+
+
