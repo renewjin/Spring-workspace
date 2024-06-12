62ba91ce421f1697ab498b9456e8d1cbbcc88d7b
오재진 <130115836+renewjin@users.noreply.github.com>
2024-06-12 13:09:47 +0900

    parma 내용 정리

---
+++ dempProject4/src/main/java/com/example/demo/DempProject4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DempProject4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DempProject4Application.class, args);
+	}
+
+}
